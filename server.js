4fb83a607dcea6d8b0948a005b86cbe75e2437fc
pawan3s <pawan29s123@gmail.com>
2022-01-20 12:35:42 +0545

    socket added

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 dotenv.config();
+const http = require("http");
+const socketIO = require("socket.io");
 
 const app = require("./app");
+
+const server = http.Server(app);
+const io = socketIO(server, {
+  cors: { origin: "https://faith-chat-9620f.web.app/" },
+});
 
 //Database
 const DB = process.env.DATABASE_URI;
@@ -20,7 +27,47 @@ mongoose
   });
 
 //server listen
-const PORT = process.env.PORT || 6000;
-const server = app.listen(PORT, () => {
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
   console.log(`Server running  on port ${PORT}`);
+});
+
+let users = [];
+
+const addUser = (userId, socketId) => {
+  !users.some((user) => user.userId === userId) &&
+    users.push({ userId, socketId });
+};
+
+const removeUser = (socketid) => {
+  users = users.filter((user) => user.socketId !== socketid);
+};
+
+const getUser = (userId) => {
+  return users.find((user) => user.userId === userId);
+};
+
+io.on("connection", (socket) => {
+  //when connected
+  console.log("a user conected");
+  socket.on("addUser", (userId) => {
+    addUser(userId, socket.id);
+    io.emit("getUsers", users);
+  });
+
+  //send and get messages
+  socket.on("sendMessage", ({ senderId, receiverId, message }) => {
+    const user = getUser(receiverId);
+    io.to(user?.socketId).emit("getMessage", {
+      senderId,
+      message,
+    });
+  });
+
+  //when disconnected
+  socket.on("disconnect", () => {
+    console.log("a user disconnected");
+    removeUser(socket.id);
+    io.emit("getUsers", users);
+  });
 });
